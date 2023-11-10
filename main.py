b9f0a3a544dce37f6b72512377fc7574bf1c38e6
frizchar <cchrysanth@gmail.com>
2023-11-10 13:19:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,19 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 
 
-population_url = "https://api.worldbank.org/v2/country/gr/indicator/SP.POP.TOTL?format=json"
-df = fad.bring_data(population_url)
-print("last 5 rows of pandas dataframe: ", "\n", df.tail(), "\n")
+if __name__ == "__main__":
+    population_url = "https://api.worldbank.org/v2/country/gr/indicator/SP.POP.TOTL?format=json"
+    df = fad.bring_data(population_url)
+    print("last 5 rows of pandas dataframe: ", "\n", df.tail(), "\n")
 
-# plot Greek population
-fig, ax = plt.subplots()
-ax.plot(df["year"], df["population"], linestyle="--", marker="o")
-first_year = min(df["year"])
-last_year = max(df["year"])
-ax.set_title(f"Greek Population from {first_year} until {last_year}", fontweight="bold")
-ax.set_xlabel("Year", fontweight='bold')
-ax.set_ylabel("Population", fontweight='bold')
-ax.xaxis.set_major_locator(MultipleLocator(7))
+    # plot Greek population
+    fig, ax = plt.subplots()
+    ax.plot(df["year"], df["population"], linestyle="--", marker="o")
+    first_year = min(df["year"])
+    last_year = max(df["year"])
+    ax.set_title(f"Greek Population from {first_year} until {last_year}", fontweight="bold")
+    ax.set_xlabel("Year", fontweight='bold')
+    ax.set_ylabel("Population", fontweight='bold')
+    ax.xaxis.set_major_locator(MultipleLocator(7))
 
-plt.show()
+    plt.show()
